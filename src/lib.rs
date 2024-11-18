84dd93b4e4a1a4ce0c18fcbe1ceefe55404d89c7
radoslaw-gajos <radoslawgajos@gmail.com>
2024-11-18 20:28:55 +0100

    Moved code to distinct modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,73 +1,81 @@
-enum ArgumentType {
+pub enum ArgumentType {
     Bool,
 }
 
-struct Schema {
-}
+mod schema {
+    use crate::ArgumentType;
 
-impl Schema {
-    fn get(c: char) -> Option<ArgumentType> {
-        None
+    pub struct Schema {
+    }
+
+    impl Schema {
+        fn get(c: char) -> Option<ArgumentType> {
+            None
+        }
     }
 }
 
-#[derive(Debug, PartialEq)]
-struct Token {
-}
+mod token {
+    use crate::ArgumentType;
 
-struct Tokens {
-}
+    #[derive(Debug, PartialEq)]
+    pub struct Token {
+    }
 
-impl Tokens {
-    fn new() -> Self {
-        Self {
+    pub struct Tokens {
+    }
+
+    impl Tokens {
+        fn new() -> Self {
+            Self {
+            }
+        }
+
+        fn size(&self) -> usize {
+            0usize
         }
     }
 
-    fn size(&self) -> usize {
-        0usize
+    pub struct TokenParser {
     }
-}
 
-struct TokenParser {
-}
+    impl TokenParser {
+        fn new(args: Vec<&str>) -> Self {
+            Self {
+            }
+        }
 
-impl TokenParser {
-    fn new(args: Vec<&str>) -> Self {
-        Self {
+        fn collect(mut self) -> Tokens {
+            Tokens::new()
         }
     }
 
-    fn collect(mut self) -> Tokens {
-        Tokens::new()
-    }
-}
+    #[cfg(test)]
+    mod tests {
+        use super::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+        #[test]
+        fn should_return_empty_token_collection_when_no_args() {
+            // given
+            let mut parser = TokenParser::new(Vec::new());
 
-    #[test]
-    fn should_return_empty_token_collection_when_no_args() {
-        // given
-        let mut parser = TokenParser::new(Vec::new());
+            // when
+            let tokens = parser.collect();
+            
+            // then
+            assert_eq!(tokens.size(), 0);
+        }
 
-        // when
-        let tokens = parser.collect();
-        
-        // then
-        assert_eq!(tokens.size(), 0);
-    }
+        #[test]
+        fn should_ignore_first_argument() {
+            // given
+            let mut parser = TokenParser::new(vec!["app_name"]);
 
-    #[test]
-    fn should_ignore_first_argument() {
-        // given
-        let mut parser = TokenParser::new(vec!["app_name"]);
-
-        // when
-        let tokens = parser.collect();
-        
-        // then
-        assert_eq!(tokens.size(), 0);
+            // when
+            let tokens = parser.collect();
+            
+            // then
+            assert_eq!(tokens.size(), 0);
+        }
     }
 }
