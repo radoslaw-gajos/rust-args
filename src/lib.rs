0fbd896d67dc03c9c056acb6732dee43e250f329
radoslaw-gajos <radoslawgajos@gmail.com>
2024-11-18 21:02:06 +0100

    getting boolean argument from schema

--- src/lib.rs
+++ src/lib.rs
@@ -5,18 +5,40 @@ pub enum ArgumentType {
 
 mod schema {
     use crate::ArgumentType;
+    use std::collections::HashMap;
 
     pub struct Schema {
+        map: HashMap<String, String>,
     }
 
     impl Schema {
         fn new() -> Self {
             Self {
+                map: HashMap::new(),
+            }
+        }
+
+        fn from(entries: Vec<(String, String)>) -> Self {
+            let map = entries
+                .into_iter()
+                .collect();
+
+            Self {
+                map,
             }
         }
 
         fn get(&self, c: char) -> Option<ArgumentType> {
-            None
+            let key = c.to_string();
+
+            if let Some(t) = self.map.get(&key) {
+                match t.as_str() {
+                    "bool" => Some(ArgumentType::Bool),
+                    &_ => panic!("invalid argument!"),
+                }
+            } else {
+                None
+            }
         }
     }
 
@@ -34,6 +56,21 @@ mod schema {
 
             // then
             assert_eq!(arg, None);
+        }
+
+        #[test]
+        fn should_return_bool_argument() {
+            // given
+            let schema = Schema::from(vec![
+                ("b".to_string(), "bool".to_string()),
+            ]);
+
+            // when
+            let arg = schema.get('b');
+
+            // then
+            assert!(arg.is_some());
+            assert_eq!(arg, Some(ArgumentType::Bool));
         }
     }
 }
