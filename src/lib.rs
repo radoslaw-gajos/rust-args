f04b98d0b55d3c596036cb1a858bee2b869db34c
radoslaw-gajos <radoslawgajos@gmail.com>
2024-11-18 20:07:19 +0100

    Returning empty token vector

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,34 @@
+#[derive(Debug, PartialEq)]
+struct Token {
+}
 
+struct TokenParser {
+}
+
+impl TokenParser {
+    fn new(args: Vec<&str>) -> Self {
+        Self {
+        }
+    }
+
+    fn collect(mut self) -> Vec<Token> {
+        Vec::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_return_empty_list_when_no_args() {
+        // given
+        let mut parser = TokenParser::new(Vec::new());
+
+        // when
+        let tokens = parser.collect();
+        
+        // then
+        assert_eq!(tokens, Vec::new());
+    }
+}
