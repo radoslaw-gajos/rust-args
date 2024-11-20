62187676f0573ad0f2e191a5cc9d8d89b43ecac2
radoslaw-gajos <radoslawgajos@gmail.com>
2024-11-20 22:19:18 +0100

    Moved argument type creation logic to it's own module

--- src/schema.rs
+++ src/schema.rs
@@ -1,10 +1,47 @@
 use std::collections::HashMap;
+use crate::schema::argument::ArgumentType;
+use crate::schema::argument::ArgumentTypeFactory;
 
-#[derive(Debug, PartialEq)]
-pub enum ArgumentType {
-    Bool,
-    Str,
-    Int,
+pub mod argument {
+    #[derive(Debug, PartialEq)]
+    pub enum ArgumentType {
+        Bool,
+        Str,
+        Int,
+    }
+
+    impl ArgumentType {
+        fn from(string: &str) -> Option<Self> {
+            match string {
+                "bool" => Some(ArgumentType::Bool),
+                "string" => Some(ArgumentType::Str),
+                "int" => Some(ArgumentType::Int),
+                &_ => panic!("invalid argument!"),
+            }
+        }
+    }
+
+    pub trait ArgumentTypeFactory {
+        fn arg_type(self: Self) -> Option<ArgumentType>;
+    }
+
+    impl ArgumentTypeFactory for &str {
+        fn arg_type(self: Self) -> Option<ArgumentType> {
+            ArgumentType::from(self)
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn should_get_type_from_string() {
+            assert_eq!(ArgumentType::from("bool"), Some(ArgumentType::Bool));
+            assert_eq!(ArgumentType::from("string"), Some(ArgumentType::Str));
+            assert_eq!(ArgumentType::from("int"), Some(ArgumentType::Int));
+        }
+    }
 }
 
 pub struct Schema {
@@ -31,16 +68,7 @@ impl Schema {
     fn get(&self, c: char) -> Option<ArgumentType> {
         let key = c.to_string();
 
-        if let Some(t) = self.map.get(&key) {
-            match t.as_str() {
-                "bool" => Some(ArgumentType::Bool),
-                "string" => Some(ArgumentType::Str),
-                "int" => Some(ArgumentType::Int),
-                &_ => panic!("invalid argument!"),
-            }
-        } else {
-            None
-        }
+        self.map.get(&key)?.arg_type()
     }
 }
 
