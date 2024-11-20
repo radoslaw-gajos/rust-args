00b7f9798bc6d7a95c79c847f392cc139d970c33
radoslaw-gajos <radoslawgajos@gmail.com>
2024-11-20 22:25:09 +0100

    Moved argument logic to separate file

--- src/schema.rs
+++ src/schema.rs
@@ -2,47 +2,7 @@ use std::collections::HashMap;
 use crate::schema::argument::ArgumentType;
 use crate::schema::argument::ArgumentTypeFactory;
 
-pub mod argument {
-    #[derive(Debug, PartialEq)]
-    pub enum ArgumentType {
-        Bool,
-        Str,
-        Int,
-    }
-
-    impl ArgumentType {
-        fn from(string: &str) -> Option<Self> {
-            match string {
-                "bool" => Some(ArgumentType::Bool),
-                "string" => Some(ArgumentType::Str),
-                "int" => Some(ArgumentType::Int),
-                &_ => panic!("invalid argument!"),
-            }
-        }
-    }
-
-    pub trait ArgumentTypeFactory {
-        fn arg_type(self: Self) -> Option<ArgumentType>;
-    }
-
-    impl ArgumentTypeFactory for &str {
-        fn arg_type(self: Self) -> Option<ArgumentType> {
-            ArgumentType::from(self)
-        }
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-
-        #[test]
-        fn should_get_type_from_string() {
-            assert_eq!(ArgumentType::from("bool"), Some(ArgumentType::Bool));
-            assert_eq!(ArgumentType::from("string"), Some(ArgumentType::Str));
-            assert_eq!(ArgumentType::from("int"), Some(ArgumentType::Int));
-        }
-    }
-}
+pub mod argument;
 
 pub struct Schema {
     map: HashMap<String, String>,
---
+++ src/schema/argument.rs
@@ -0,0 +1,40 @@
+#[derive(Debug, PartialEq)]
+pub enum ArgumentType {
+    Bool,
+    Str,
+    Int,
+}
+
+impl ArgumentType {
+    fn from(string: &str) -> Option<Self> {
+        match string {
+            "bool" => Some(ArgumentType::Bool),
+            "string" => Some(ArgumentType::Str),
+            "int" => Some(ArgumentType::Int),
+            &_ => panic!("invalid argument!"),
+        }
+    }
+}
+
+pub trait ArgumentTypeFactory {
+    fn arg_type(self: Self) -> Option<ArgumentType>;
+}
+
+impl ArgumentTypeFactory for &str {
+    fn arg_type(self: Self) -> Option<ArgumentType> {
+        ArgumentType::from(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_get_type_from_string() {
+        assert_eq!(ArgumentType::from("bool"), Some(ArgumentType::Bool));
+        assert_eq!(ArgumentType::from("string"), Some(ArgumentType::Str));
+        assert_eq!(ArgumentType::from("int"), Some(ArgumentType::Int));
+    }
+}
+
