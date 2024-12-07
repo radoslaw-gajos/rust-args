eabaffcbb4e84678ea387bfa0b0fbde3950f2b73
radoslaw-gajos <radoslawgajos@gmail.com>
2024-12-07 19:47:03 +0100

    Adding indexing to tokens

--- src/token/tokens.rs
+++ src/token/tokens.rs
@@ -5,6 +5,7 @@ use crate::schema::Schema;
 pub struct Tokens {
     items: Vec<Token>,
     schema: Option<Schema>,
+    index: usize,
 }
 
 impl Tokens {
@@ -12,6 +13,7 @@ impl Tokens {
         Self {
             schema: Some(schema),
             items: Vec::new(),
+            index: 0,
         }
     }
 
@@ -33,5 +35,26 @@ impl Tokens {
 
     pub fn schema_set(&mut self, schema: Schema) {
         self.schema = Some(schema);
+    }
+
+    pub fn current(&self) -> Option<&Token> {
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_get_none_if_no_more_tokens() {
+        // given
+        let tokens = Tokens::default();
+
+        // when
+        let current = tokens.current();
+
+        // then
+        assert_eq!(current, None);
     }
 }
