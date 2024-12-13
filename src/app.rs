de804918edb066992b145a34d1dc2a2905b81222
radoslaw-gajos <radoslawgajos@gmail.com>
2024-12-13 18:55:32 +0100

    Added template methods for application

--- src/app.rs
+++ src/app.rs
@@ -3,6 +3,14 @@ struct App {
 
 impl App {
     pub fn run(&self) {
+        self.init();
+        let col = self.get_collection();
+    }
+
+    fn init(&self) {
+    }
+
+    fn get_collection(&self) -> Collection {
     }
 }
 
